ff2495d4f4df2d1914fa64b291f28805fa1fbb8b
jomonkjoy <jomonkjoy@localhost.localdomain>
2023-09-02 17:55:24 -0700

    Register file to map to BRAM

--- src/rv32i_register_file.sv
+++ src/rv32i_register_file.sv
@@ -15,24 +15,56 @@ module rv32i_register_file (
     input  logic            rst_n
 );
 
-    reg [31:0] RF_REGISTERS [31:0];
+    // Synthesis attributes to map to Block RAM
+    (* ram_style = "block" *)
+    (* use_bram = "yes" *)
+    (* use_bram_for_single_port = "yes" *)
+    logic [31:0]    RV32I_RF_A [31:0];
+    // Synthesis attributes to map to Block RAM
+    (* ram_style = "block" *)
+    (* use_bram = "yes" *)
+    (* use_bram_for_single_port = "yes" *)
+    logic [31:0]    RV32I_RF_B [31:0];
+
+    logic [31:0]    read_data1_reg;
+    logic           read_reg1_zero;
+    logic [31:0]    read_data2_reg;
+    logic           read_reg2_zero;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            // Initialize RF_REGISTERS to zero on reset
-            for (int i = 0; i < 32; i = i + 1) begin
-                RF_REGISTERS[i] <= 32'h00000000;
-            end
-        end else if (write_enable) begin
+            read_reg1_zero <= 1'b1;
+            read_reg2_zero <= 1'b1;
+        end else begin
+            read_reg1_zero <= (read_reg1 == 0);
+            read_reg2_zero <= (read_reg2 == 0);
+        end
+    end
+
+    // Register file A definition
+    always @(posedge clk) begin
+        read_data1_reg <= RV32I_RF_A[read_reg1];
+        if (write_enable) begin
             // Write to the register file
             if (write_reg != 0) begin
-                RF_REGISTERS[write_reg] <= write_data;
+                RV32I_RF_A[write_reg] <= write_data;
             end
         end
     end
 
-    assign read_data1 = (read_reg1 == 0) ? 32'h00000000 : RF_REGISTERS[read_reg1];
-    assign read_data2 = (read_reg2 == 0) ? 32'h00000000 : RF_REGISTERS[read_reg2];
+    // Register file B definition
+    always @(posedge clk) begin
+        read_data2_reg <= RV32I_RF_B[read_reg2];
+        if (write_enable) begin
+            // Write to the register file
+            if (write_reg != 0) begin
+                RV32I_RF_B[write_reg] <= write_data;
+            end
+        end
+    end
+
+    assign read_data1 = read_reg1_zero ? 32'h00000000 : read_data1_reg;
+    assign read_data2 = read_reg2_zero ? 32'h00000000 : read_data2_reg;
 
 endmodule
 

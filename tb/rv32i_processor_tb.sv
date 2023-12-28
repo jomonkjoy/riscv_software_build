250dfa72046fef2911e2545a0b8f13519379ff7c
Design@vlsi <jomonkurianjoy@gmail.com>
2023-12-27 19:12:55 -0800

    Add files via upload

--- tb/rv32i_processor_tb.sv
+++ tb/rv32i_processor_tb.sv
@@ -56,6 +56,26 @@ module rv32i_processor_tb(
         .reset(reset)
     );
     
+    // Instantiate the memory module
+    my_memory #(.INIT_FILE("instr_mem.mem")) instruction_memory (
+        .clk(clk),
+        .address(instruction_address[11:2]),
+        .write_data('0),
+        .write_byteenable('0),
+        .write_enable('0),
+        .read_data(instruction_data[31:0])
+    );
+        
+    // Instantiate the memory module
+    my_memory #(.INIT_FILE("data_mem.mem")) data_memory (
+        .clk(clk),
+        .address(memory_address[11:2]),
+        .write_data(memory_write_data[31:0]),
+        .write_byteenable(memory_byteenable[3:0]),
+        .write_enable(memory_write),
+        .read_data(memory_read_data[31:0])
+    );
+    
     // Print some stuff as an example
     initial begin
         $display("[%0t] Model running...\n", $time);
@@ -78,4 +98,39 @@ module rv32i_processor_tb(
         // Add stimulus here
     end
 
+endmodule
+
+module my_memory #(
+  parameter INIT_FILE = ""
+ ) (
+  input logic [9:0] address,
+  input logic [31:0] write_data,
+  input logic [3:0] write_byteenable,
+  input logic write_enable,
+  output logic [31:0] read_data,
+  input logic clk
+);
+  // Memory definition
+  logic [31:0] mem [0:1023];
+
+  // Initial block to initialize memory
+  initial begin
+    $readmemh(INIT_FILE, mem);
+    // Additional simulation setup code goes here
+  end
+  
+  // Memory read and write logic
+  always_ff @(posedge clk) begin
+    if (write_enable) begin
+      // Write operation
+      for (int i=0; i<4; i++) begin
+        if (write_byteenable[i]) begin
+          mem[address][i*8 -: 8] <= write_data[i*8 -: 8];
+        end
+      end
+    end else begin
+      // Read operation
+      read_data <= mem[address];
+    end
+  end
 endmodule
